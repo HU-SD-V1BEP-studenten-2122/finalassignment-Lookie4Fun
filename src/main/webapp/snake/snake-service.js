f12436a838f59dff03465af6f0d9b3d31757b7f7
Nick <nick.deheij@student.hu.nl>
2022-06-23 21:31:50 +0200

    eerste pusj

--- src/main/webapp/snake/snake-service.js
+++ src/main/webapp/snake/snake-service.js
@@ -1,18 +1,26 @@
 export default class SnakeService {
+
     async getSnake() {
-        //TODO: haal deze data van de server
-        return Promise.resolve({
-            apiversion: "1",
-            author: "de dapper student",
-            color: "#ff0000",
-            head: "default",
-            tail: "default",
-            version: "0.1"
-        });
+
+        fetch('https://battlesnake-lookie4fun.herokuapp.com/restservices/bepslang')
+            .then((response) => {
+                if (!response.ok) {
+                    throw new Error(response.error)
+                }
+                return Promise.resolve(response.json());
+            })
     }
 
     async updateSnake(updatedSnake) {
         //TODO: update je slang aan de server-kant met de nieuwe gegevens
-        return Promise.resolve();
+        fetch('https://battlesnake-lookie4fun.herokuapp.com/restservices/bepslang', {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(updatedSnake)
+        }).then(response => {return response.json()})
+            .catch(error => console.log('ERROR'));
     }
 }
+
