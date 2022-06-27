439f988f29ae134ad339e5b059037f9d12fdaf1c
Nick <nick.deheij@student.hu.nl>
2022-06-27 09:39:36 +0200

    Snake-service werkt

--- src/main/webapp/snake/snake-service.js
+++ src/main/webapp/snake/snake-service.js
@@ -1,13 +1,13 @@
 export default class SnakeService {
 
     async getSnake() {
-        return fetch('https://battlesnake-lookie4fun.herokuapp.com/restservices/bepslang')
+        return fetch('https://testslang1.herokuapp.com/restservices/bepslang')
             .then(response => response.json())
 
     }
     async updateSnake(updatedSnake) {
         //TODO: update je slang aan de server-kant met de nieuwe gegevens
-        return fetch('https://battlesnake-lookie4fun.herokuapp.com/restservices/bepslang/update', {
+        return fetch('https://testslang1.herokuapp.com/restservices/bepslang/update', {
             method: 'PATCH',
             headers: {
                 'Content-Type': 'application/json'
